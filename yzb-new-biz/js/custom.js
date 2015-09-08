3f8dd4da8b24a8f6827802e1bb1172c8249f849e
Gao Xin <gaoxin@dianlike.com>
2015-09-08 13:55:36 +0800

    Improve page effects, and animate for buttons and create new tips animate.

--- yzb-new-biz/js/custom.js
+++ yzb-new-biz/js/custom.js
@@ -1,26 +1,57 @@
 $(function(){
-	$('.joke-content-wrap .joke-list dd.joke-content a.like-btn').click(function(){
-		$(this).toggleClass('liked');
-	});
+    $('.joke-content-wrap .joke-list dd.joke-content a.like-btn').click(function(){
+        $(this).toggleClass('liked');
+    });
 
-	function showCheckInSuccessTips() {
-		$('.check-in-tips').removeClass('hidden');
-		$(this).addClass('checked-style');
-		$('.check-in-tips').animate({
-			top: 0,
-			opacity: 1},
-			500, function() {
-				setTimeout(
-					function(){
-						$('.check-in-tips').animate({
-							top: '-60px',
-							opacity: 0},
-							500, function() {
-							$('.check-in-tips').addClass('hidden');
-						});
-					}, 1500);
-		});
-	}
+    //Show check-in success tips function
+    function showCheckInSuccessTips() {
+        var checkInTips = $('.check-in-tips');
+        checkInTips.removeClass('hidden');
+        $(this).addClass('checked-style');
+        checkInTips.animate({
+            top: 0,
+            opacity: 1},
+            500, function() {
+                setTimeout(
+                    function(){
+                        checkInTips.animate({
+                            top: '-60px',
+                            opacity: 0},
+                            500, function() {
+                            checkInTips.addClass('hidden');
+                        });
+                    }, 1500);
+        });
+    }
+    $('.check-in-wrap .check-in-btn').one('click', showCheckInSuccessTips);
 
-	$('.check-in-wrap .check-in-btn').one('click', showCheckInSuccessTips);
+    //Show fault repiar tips function
+    function showFaultRepairTips() {
+        var faultTips = $('.fault-repair-tips');
+        faultTips.removeClass('hidden');
+        faultTips.animate({
+            top: 0,
+            opacity: 1},
+            1000, function() {
+                setTimeout(
+                    function(){
+                        faultTips.animate({
+                            top: '-60px',
+                            opacity: 0},
+                            500, function() {
+                            faultTips.addClass('hidden');
+                        });
+                    }, 1500);
+        });
+    }
+    $('.fault-repair-btn').click(showFaultRepairTips);
+
+    //The loading effect for buttons
+    Ladda.bind( '#exchangeModal button', { timeout: 2000 } );
+    Ladda.bind( 'button.reserve-btn', { timeout: 2000 } );
+    $('button.reserve-btn').click(function(){
+        setTimeout(function(){
+            window.location.href='../reserve-success.html'
+        }, 2500);
+    });
 });
